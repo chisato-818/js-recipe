3bce568d1e7d710d956c9f9af860a9948c29d28a
chisato-818 <chisa818.milkyyy@gmail.com>
2021-10-13 00:33:22 +0900

    メモアプリ

--- 04_ゼミ/week2/メモアプリ/main.js
+++ 04_ゼミ/week2/メモアプリ/main.js
@@ -4,17 +4,22 @@ const addButton = document.getElementById("add-button")
 
 // ボタンを押したらテキストが下に追加される
 addButton.onclick = function() {
-  const card = document.createElement("div")
-  card.className = "card"
-  card.textContent = inputElement.value
-
   if (inputElement.value !== "") {
+    const container = document.createElement("div")
+    container.className = "container"
+
+    const card = document.createElement("div")
+    card.className = "card"
+    card.textContent = inputElement.value
+
     const sakuzyo = document.createElement("div")
     sakuzyo.className = "sakuzyo"
     sakuzyo.textContent = "削除"
 
-    cardsContainer.appendChild(card)
-    cardsContainer.appendChild(sakuzyo)
+    cardsContainer.appendChild(container)
+    container.appendChild(card)
+    container.appendChild(sakuzyo)
+
     inputElement.value = ""
 
     sakuzyo.onclick = function() {
